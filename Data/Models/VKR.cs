1cac7060222a7237a202f783e97cd36dc761b48f
TitaniumLexa <titanium.lexa@gmail.com>
2020-02-10 19:30:34 +0300

    Обновил модель VKR. Добавлены: Ссылки на специальные профили для Студента и Научного руководителя. Год и семестр ВКР.

--- Data/Models/VKR.cs
+++ Data/Models/VKR.cs
@@ -4,21 +4,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using FinalWork_BD_Test.Data.Models.Base;
+using FinalWork_BD_Test.Data.Models.Data;
 using FinalWork_BD_Test.Data.Models.Profiles;
 
 namespace FinalWork_BD_Test.Data.Models
 {
     public class VKR : HistoricalModelBase<VKR>
     {
-        /* Есть множество способов обеспечить связь с данными
-         * от хранения ссылок на профили, момента последнего обновления
-         * так определение профиля от времени обновления и пользователя
-         */
-        //public User Student { get; set; }
         public UserProfile StudentUP { get; set; }
+        public StudentProfile StudentSP { get; set; }
         public Topic Topic { get; set; }
-        //public User Supervisor { get; set; }
         public UserProfile SupervisorUP { get; set; }
-        //public User Reviewer { get; set; }
+        public LecturerProfile SupervisorLP { get; set; }
+        
+        /// <summary>
+        /// Рецензент, для магистров
+        /// </summary>
+        public UserProfile ReviewerUP { get; set; }
+
+        public DateTime Year { get; set; }
+        public Semester Semester { get; set; }
+
     }
 }
